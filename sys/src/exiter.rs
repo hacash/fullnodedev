6697b860e6912f1f012f3c6e814c5f11a1afe04f
jojoin <2978963+jojoin@users.noreply.github.com>
2026-01-19 10:38:26 +0800

    up

--- sys/src/exiter.rs
+++ sys/src/exiter.rs
@@ -93,7 +93,8 @@ impl Exiter {
     
     pub fn exit(&self) {
         // broadcast to nitify all thread to quit
-        let _ = self.sender.broadcast(());
+        let _ = self.sender.broadcast_blocking(());
+        // let _ = self.sender.broadcast(());
     }
     
     pub fn wait(self) {
