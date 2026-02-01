32a44ad2054f6adff905166227ce6fee2dd1c5ea
jojoin <2978963+jojoin@users.noreply.github.com>
2026-02-01 14:34:08 +0800

    use chain v2

--- chain/src/lib.rs
+++ chain/src/lib.rs
@@ -1,9 +1,36 @@
+use std::sync::*;
+use std::ops::*;
+use std::sync::atomic::*;
+use std::collections::*;
+use std::path::PathBuf;
+use std::thread::sleep;
+use std::time::Duration;
+
+use sys::*;
+use field::*;
+use basis::interface::*;
+use basis::component::*;
+use basis::config::*;
+use protocol::state::*;
+use protocol::transaction::*;
+use protocol::block::{self, BlockHeadOnlyHeight, create_tx_info};
+use protocol::context as ctx;
 
 
-pub mod engine;
 
 
-
+include!{"count.rs"}
+include!{"state.rs"}
+include!{"verify.rs"}
+include!{"chunk.rs"}
+include!{"tree.rs"}
+include!{"init.rs"}
+include!{"check.rs"}
+include!{"insert.rs"}
+include!{"sync.rs"}
+include!{"lock.rs"}
+include!{"engine.rs"}
+include!{"impl.rs"}
 
 
 
--- chain/src/state/mod.rs
+++
@@ -1,5 +0,0 @@
-
-
-include!{"state.rs"}
-
-
