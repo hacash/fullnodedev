4c5ca8af6d2659b858611a7935e9d2ff740bc104
jojoin <2978963+jojoin@users.noreply.github.com>
2026-04-18 23:48:22 +0800

    + ASERT v1.0.3

--- app/src/version.rs
+++ app/src/version.rs
@@ -1,9 +1,9 @@
 
 
 #[allow(unused)]
-pub const HACASH_NODE_VERSION: &str    = "1.0.2";
+pub const HACASH_NODE_VERSION: &str    = "1.0.3";
 #[allow(unused)]
-pub const HACASH_NODE_BUILD_TIME: &str = "2026/4/17 #1";
+pub const HACASH_NODE_BUILD_TIME: &str = "2026/4/18 #1";
 #[allow(unused)]
 pub const HACASH_STATE_DB_UPDT: u32    = 6;
 
