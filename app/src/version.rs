3b6dc5f0f2098619c4e095f958430cde0b2c6eb2
jojoin <2978963+jojoin@users.noreply.github.com>
2026-06-23 14:48:18 +0800

    + 1.0.6

--- app/src/version.rs
+++ app/src/version.rs
@@ -1,9 +1,9 @@
 
 
 #[allow(unused)]
-pub const HACASH_NODE_VERSION: &str    = "1.0.5";
+pub const HACASH_NODE_VERSION: &str    = "1.0.6";
 #[allow(unused)]
-pub const HACASH_NODE_BUILD_TIME: &str = "2026/6/15 #1";
+pub const HACASH_NODE_BUILD_TIME: &str = "2026/6/22 #1";
 #[allow(unused)]
 pub const HACASH_STATE_DB_UPDT: u32    = 7;
 
